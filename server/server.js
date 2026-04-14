fc806e1fe9f19187bfe54964f8e7e6d5cb23a88d
Manikanta-04 <238w1a12a7@vrsec.ac.in>
2026-04-14 18:58:20 +0530

    fixed the server.js

--- server/server.js
+++ server/server.js
@@ -59,7 +59,6 @@ app.use((req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("🚀 Server running on port " + PORT);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
